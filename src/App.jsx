8b2b22e3fe1056be0ada9308b9a9ed42886ab8b8
Aj-Herrera-99 <ajherrera999@gmail.com>
2024-12-18 16:23:37 +0100

    multiField

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 /**
 
-Aggiungere più campi al form (ad es. lo stato di un articolo - draft, published - o l’autore)
+ * todo: Aggiungere più campi al form (ad es. lo stato di un articolo - draft, published - o l’autore)
  
 */
 
@@ -13,6 +13,8 @@ import { useState } from "react";
 
 const initialData = {
     title: "",
+    author: "",
+    status: false,
 };
 
 function App() {
@@ -22,14 +24,28 @@ function App() {
 
     // actions
     const handleSubmit = (e) => {
+        console.log("test");
         e.preventDefault();
         setCards([...cards, formData]);
         setFormData(initialData);
     };
 
-    const handleInputChange = (e) => {
-        // console.log(e.target.value);
+    const handleTitleChange = (e) => {
         setFormData({ ...formData, title: e.target.value });
+    };
+    const handleAuthorChange = (e) => {
+        setFormData({ ...formData, author: e.target.value });
+    };
+    const handleStatusChange = (e) => {
+        setFormData({ ...formData, status: e.target.checked });
+    };
+
+    const handleInputChange = (e) => {
+        console.log(e.target);
+        const { type, name, value, checked } = e.target;
+        const KEY = name;
+        const VAL = type == "checkbox" ? checked : value;
+        setFormData({ ...formData, [KEY]: VAL });
     };
 
     const handleRemoveClick = (e) => {
@@ -38,6 +54,7 @@ function App() {
         const nuoveCards = cards.filter((card, index) => index != cardId);
         setCards(nuoveCards);
     };
+
     return (
         <>
             {/* Header */}
@@ -50,13 +67,38 @@ function App() {
             <main>
                 {/* Form */}
                 <section className="bg-green-300">
-                    <form action="#" onSubmit={handleSubmit}>
+                    <form
+                        action="#"
+                        onSubmit={handleSubmit}
+                        className="flex flex-col gap-4"
+                    >
                         <input
                             type="text"
+                            name="title"
                             className="p-4 border border-blue-300"
                             onChange={handleInputChange}
                             value={formData.title}
+                            placeholder="Titolo"
                         />
+                        <input
+                            type="text"
+                            name="author"
+                            className="p-4 border border-blue-300"
+                            onChange={handleInputChange}
+                            value={formData.author}
+                            placeholder="Autore"
+                        />
+                        <label htmlFor="status">Articolo pubblicato?</label>
+                        <input
+                            type="checkbox"
+                            name="status"
+                            className="p-4 border border-blue-300"
+                            onChange={handleInputChange}
+                            checked={formData.status}
+                        />
+                        <button type="submit" className="px-4 bg-blue-300 y-2">
+                            Invia
+                        </button>
                     </form>
                 </section>
                 {/* CardWrapper */}
@@ -70,6 +112,11 @@ function App() {
                             >
                                 <div className="flex flex-col">
                                     <span>Titolo: {card.title}</span>
+                                    <span>Author: {card.author}</span>
+                                    <span>
+                                        Status:{" "}
+                                        {card.status ? "Pubblicato" : "Bozza"}
+                                    </span>
                                     <span
                                         onClick={handleRemoveClick}
                                         className="text-red-500 cursor-pointer hover:scale-105"
